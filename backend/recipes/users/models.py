fdf806654fbe32a2c8e5e6822dfcf5cf23a80be8
Stanislav <stas.kh@inbox.ru>
2022-10-13 20:12:37 +0300

    Создал проект и приложения api и users

---
+++ backend/recipes/users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        blank=True,
+        unique=True,
+        verbose_name='Адрес электронной почты',
+    )
+    username = models.CharField(
+        max_length=150,
+        null=True,
+        unique=True,
+        verbose_name='Имя пользователя'
+    )
+    first_name = models.CharField(
+        max_length=150, blank=True, verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150, blank=True, verbose_name='Фамилия'
+    )
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name_plural = "Пользователи"
+        verbose_name = "Пользователь"
