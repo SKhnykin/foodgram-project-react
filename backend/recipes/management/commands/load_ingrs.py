dbb816e025b89515434e0ce968101dc067d4f98d
Cham <stas.kh@inbox.ru>
2022-11-17 22:26:53 +0300

    поправил

--- backend/recipes/management/commands/load_ingrs.py
+++ backend/recipes/management/commands/load_ingrs.py
@@ -1,24 +1,25 @@
 import csv
 
 from django.conf import settings
-from django.core.management import BaseCommand
-
+from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
-
 class Command(BaseCommand):
-    help = 'Загрузка из csv файла'
+    help = 'Загрузка ингредиентов в БД из .csv'
 
-    def handle(self, *args, **kwargs):
+    def handle(self, **kwargs):
         data_path = settings.BASE_DIR
         with open(
             f'{data_path}/data/ingredients.csv',
             'r',
-            encoding='utf-8'
+            encoding='UTF-8'
         ) as file:
-            reader = csv.DictReader(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader)
-        self.stdout.write(
-            self.style.SUCCESS('Все данные об ингредиентах загружены!')
-        )
+            reader = csv.reader(file, delimiter=',')
+            for row in reader:
+                Ingredient.objects.get_or_create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
+        self.stdout.write(self.style.SUCCESS(
+                          'Ингредиенты успешно загружены в БД.'
+                          ))
